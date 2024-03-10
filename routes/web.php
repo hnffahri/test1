1a272d34ba67e505c158f73e8a2b4cec1bfac0aa
hnffahri <haniffahri155@gmail.com>
2024-03-10 23:05:09 +0700

    middleware session

--- routes/web.php
+++ routes/web.php
@@ -37,15 +37,15 @@ use Illuminate\Support\Facades\Route;
 // Route::get('siswa', [SiswaController::class, 'index']);
 // Route::get('siswa/{id}', [SiswaController::class, 'detail'])->where('id', '[0-9]+');
 
-Route::resource('siswa', SiswaController::class);
+Route::resource('siswa', SiswaController::class)->middleware('isLogin');
 
 Route::get('/', [HalamanController::class, 'index']);
 Route::get('/tentang', [HalamanController::class, 'tentang']);
 Route::get('/kontak', [HalamanController::class, 'kontak']);
 
-Route::get('/sesi', [SessionController::class, 'index']);
+Route::get('/sesi', [SessionController::class, 'index'])->middleware('isTamu');
 Route::post('/sesi/login', [SessionController::class, 'login']);
 Route::get('/sesi/logout', [SessionController::class, 'logout']);
 
-Route::get('/sesi/register', [SessionController::class, 'register']);
+Route::get('/sesi/register', [SessionController::class, 'register'])->middleware('isTamu');
 Route::post('/sesi/create', [SessionController::class, 'create']);
