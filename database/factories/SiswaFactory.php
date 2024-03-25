06be68e4af5d8cc3d0ba4d0e7edf9ceea9766b9e
hnffahri <haniffahri155@gmail.com>
2024-03-25 21:39:47 +0700

    fake data siswa

---
+++ database/factories/SiswaFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Siswa>
+ */
+class SiswaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nama' => fake()->name(),
+            'no_induk' => fake()->unique()->randomNumber(9, true),
+            'alamat' => fake()->address(),
+            'foto' => fake()->regexify()
+        ];
+    }
+}
--- database/seeders/SiswaSeeder.php
+++ database/seeders/SiswaSeeder.php
@@ -13,27 +13,6 @@ class SiswaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-        DB::table('siswa')->insert([
-            'nama' => 'Anis',
-            'no_induk' => '3987123',
-            'alamat' => 'Bantul',
-            'created_at' => date('Y-m-d H:i:s'),
-            'foto' => ''
-        ]);
-        DB::table('siswa')->insert([
-            'nama' => 'Bowo',
-            'no_induk' => '3987124',
-            'alamat' => 'Cirebon',
-            'created_at' => date('Y-m-d H:i:s'),
-            'foto' => ''
-        ]);
-        DB::table('siswa')->insert([
-            'nama' => 'Ganjar',
-            'no_induk' => '3987125',
-            'alamat' => 'Pnd',
-            'created_at' => date('Y-m-d H:i:s'),
-            'foto' => ''
-        ]);
+        \App\Models\Siswa::factory(10)->create();
     }
 }
